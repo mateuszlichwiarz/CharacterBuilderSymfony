cdbd877aac3865158ded147afcdd29a52f6c513e
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-17 15:00:30 +0200

    temporary methods

--- app/src/Service/Equipment/ArmorEquipment.php
+++ app/src/Service/Equipment/ArmorEquipment.php
@@ -5,43 +5,30 @@ namespace App\Service\Equipment;
 
 use App\Service\Equipment\AbstractEquipment;
 use App\Repository\ArmorRepository;
+use App\Repository\CharacterRepository;
 
 
 class ArmorEquipment extends AbstractEquipment
 {
-    private ArmorRepository $armorRepository;
-    private CharacterRepository $characterRepository;
-    
-    public function __construct(ArmorRepository $armorRepository, CharacterRepository $characterRepository)
-    {
-        $this->armorRepository = $armorRepository;
-        $this->characterRepository = $characterRepository;
-    }
 
-    private function findEquipmentById()
-    {
-        $armor = $this->armorRepository->findArmorById($id);
-        if($armor === null)
-        {
-            throw new \Exception(sprintf('Armor with this id doesnt exist'));
-        }else
-        {
-            return $armor;
-        }
-    }
+    public function __construct(
+        private ArmorRepository $armorRepository,
+        private CharacterRepository $characterRepository
+        )
+    {}
 
     public function putOnByName($name)
     {
-        $armor = $this->findEquipmentById($name);
-        $armorId = $armor->getId(); //na czuja
+        $armor = $this->findEquipmentByName($name, $this->armorRepository);
+        $armorId = $armor->getId();
         $this->characterRepository->updateCharacter($armorId, 'armor');
     }
 
     public function putOnById($id)
     {
-        $armor = $this->findEquipmentById($id);
-        $armorId = $armor->getId(); //na czuja
-        $this->characterRepository->updateCharacter($armorId, 'armor');
+        $armor = $this->findEquipmentById($id, $this->armorRepository);
+        $armorName = $armor->getName();
+        $this->characterRepository->updateCharacter($armorName, 'armor');
     }
 
     
