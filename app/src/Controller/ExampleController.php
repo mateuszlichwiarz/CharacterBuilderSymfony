b8bd7d0d54d2d4be79bbf00ba8c8463e4588c425
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-17 15:01:16 +0200

    example for tests

--- app/src/Controller/ExampleController.php
+++ app/src/Controller/ExampleController.php
@@ -8,18 +8,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ArmorRepository;
+use App\Repository\WeaponRepository;
+
+use App\Service\Factory\CharacterFactory;
 
 class ExampleController extends AbstractController
 {
     #[Route('/foo/bar')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(CharacterFactory $characterFactory, ArmorRepository $armorRepository, WeaponRepository $weaponRepository): Response
     {
 
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
+        $character = $user->getPlayerCharacter();
 
         $id = $user->getEmail();
+
+        $characterFactory->create($character);
+        
 
         return new Response('foo bar '.$id);
     }
