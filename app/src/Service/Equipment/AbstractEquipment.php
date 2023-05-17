6a4dbe79efc0aef88ad43b43478fa1072d760dd0
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-17 13:56:29 +0200

    find equipment methods

--- app/src/Service/Equipment/AbstractEquipment.php
+++ app/src/Service/Equipment/AbstractEquipment.php
@@ -6,4 +6,28 @@ abstract class AbstractEquipment
 {
     abstract public function putOnById($id);
     abstract public function putOnByName($name);
+
+    public function findEquipmentById(int $id, $repository): object
+    {
+        $object = $repository->findById($id);
+        if($object === null)
+        {
+            throw new \Exception(sprintf('not exist'));
+        }else
+        {
+            return $object;
+        }
+    }
+
+    public function findEquipmentByName(string $name, $repository): object
+    {
+        $object = $repository->findByName($name);
+        if($object === null)
+        {
+            throw new \Exception(sprintf('not exist'));
+        }else
+        {
+            return $object;
+        }
+    }
 }
