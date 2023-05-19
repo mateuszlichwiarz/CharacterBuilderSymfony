a3f6fa8e0261b173f151b3a0fd0317029c8b36e5
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-19 13:43:41 +0200

    shy prepare to updater classes

---
+++ app/src/Service/Updater/AbstractUpdater.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Service\Updater;
+
+Abstract class AbstractUpdater
+{
+    abstract public function update();
+}
---
+++ app/src/Service/Updater/CharacterUpdater.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service\Updater;
+
+class CharacterUpdater
+{
+    public function update()
+    {
+
+    }
+}
