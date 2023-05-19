ec23f9dc95c53c7c9d29db5ae73a2ebf77d7bfe1
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-19 02:24:17 +0200

    test: test armor class

---
+++ app/tests/Service/Equipment/ArmorEquipmentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\tests\Service\Equipment;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+use App\Service\Equipment\ArmorEquipment;
+
+class ArmorEquipmentTest extends KernelTestCase
+{
+    public function testFindArmorMethodById(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $armorEquipment = $container->get(ArmorEquipment::class);
+
+        $armorId = $armorEquipment->getArmorById(1);
+        $this->assertSame('pantaloons', $armorId);
+    }
+
+    public function testFindArmorMethodByName(): void
+    {
+        self::bootKernel();
+        
+        $container = static::getContainer();
+
+        $armorEquipment = $container->get(ArmorEquipment::class);
+
+        $armorName = $armorEquipment->getArmorByName('pantaloons');
+        $this->assertSame(1, $armorName);
+    }
+}
