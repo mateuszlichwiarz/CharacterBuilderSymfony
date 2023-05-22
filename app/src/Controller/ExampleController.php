e34932e7dba61a8610cecf955c27b57b0daa659f
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-22 04:04:24 +0200

    testing controller for factory

--- app/src/Controller/ExampleController.php
+++ app/src/Controller/ExampleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Character;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,24 +11,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use App\Repository\ArmorRepository;
 use App\Repository\WeaponRepository;
+use App\Repository\CharacterRepository;
 
 use App\Service\Factory\CharacterFactory;
 
 class ExampleController extends AbstractController
 {
+    public function __construct(
+        private CharacterFactory $characterFactory,
+        //private CharacterRepository $characterRepository
+    ){}
+
     #[Route('/foo/bar')]
-    public function index(CharacterFactory $characterFactory, ArmorRepository $armorRepository, WeaponRepository $weaponRepository): Response
+    public function index(): Response
     {
 
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
         $character = $user->getPlayerCharacter();
+        if(is_null($character))
+        {
+            $character = new Character();
+            $character->setName('foo_bar')->setStr(10);
+            $this->characterFactory->create($character);
 
-        $id = $user->getEmail();
-
-        $characterFactory->create($character);
-        
-
-        return new Response('foo bar '.$id);
+            return new Response('foo bar: saved '.$character->getId());
+        }else
+        {
+            return new Response('foo bar '.$user->getEmail());
+        }
     }
 }
