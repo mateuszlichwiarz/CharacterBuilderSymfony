cf3eee4011bbd17c126f0945c89c8c4c5ec2ffb0
mateusz_lichwiarz <mateuszlichwiarz@gmail.com>
2023-05-22 04:17:08 +0200

    test: armor equipment

--- app/tests/Service/Equipment/ArmorEquipmentTest.php
+++ app/tests/Service/Equipment/ArmorEquipmentTest.php
@@ -12,22 +12,34 @@ class ArmorEquipmentTest extends KernelTestCase
     {
         self::bootKernel();
         $container = static::getContainer();
-
         $armorEquipment = $container->get(ArmorEquipment::class);
 
         $armorId = $armorEquipment->getArmorById(1);
+
         $this->assertSame('pantaloons', $armorId);
     }
 
     public function testFindArmorMethodByName(): void
     {
         self::bootKernel();
-        
         $container = static::getContainer();
-
         $armorEquipment = $container->get(ArmorEquipment::class);
 
         $armorName = $armorEquipment->getArmorByName('pantaloons');
+
+        $this->assertSame(1, $armorName);
+    }
+
+    //i think mocking will be
+    
+    public function testPutOnByName(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $armorEquipment = $container->get(ArmorEquipment::class);
+
+        $armorName = $armorEquipment->getArmorByName('pantaloons');
+
         $this->assertSame(1, $armorName);
     }
 }
