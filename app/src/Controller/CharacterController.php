d1672e764172ef2e5ad6ef055679be8031a690d0
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-02 16:32:47 +0200

    update character controller

--- app/src/Controller/CharacterController.php
+++ app/src/Controller/CharacterController.php
@@ -5,80 +5,61 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Service\Character\CharacterManager;
+use App\Service\Character\Factory\CharacterBuilderFactory;
+
 use App\Form\Character\CreateCharacterFormType;
-use App\Service\Character\Factory\CharacterFactory;
 
-use App\Entity\User;
 use App\Entity\Character;
-
-use App\Repository\UserRepository;
 use App\Repository\CharacterRepository;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CharacterController extends AbstractController
 {
     public function __construct(
-        private CharacterFactory $characterFactory,
+        private CharacterBuilderFactory $characterBuilderFactory,
         private CharacterRepository $characterRepository,
-        private UserRepository $userRepository,
+        private CharacterManager $characterManager,
     ){}
 
-    #[Route("/character", name: 'character_index')]
-    public function index(Request $request): Response
+    #[Route("/character", name: 'character_show')]
+    public function show(Request $request): Response
     {
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-        $character = $user->getPlayerCharacter();
-        if(is_null($character))
-        {
+        $character = $this->characterManager->getCharacter();
+        if(is_null($character)) {
             return $this->render('character/index.html.twig');
-        }else
-        {
-            $character = $user->getPlayerCharacter();
-
+        }else {
             return $this->render('character/character.html.twig',[
                 'character' => $character,
             ]);
         }
     }
-
     
-    #[Route("/character/creator", name: 'character_creator')]
+    #[Route("/character/create", name: 'character_create')]
     public function create(Request $request): Response
     {
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-
         $character = new Character();
 
         $form = $this->createForm(CreateCharacterFormType::class, $character);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if($form->isSubmitted() && $form->isValid()) {
                 $character = $form->getData();
+                $character = $this->characterBuilderFactory->createBuilder(1, 1, 10);
 
-                $character = $this->characterFactory->createCharacter(1, 1, 10);
-                $this->characterRepository->save($character);
-
-                $user->setPlayerCharacter($character);
-                $this->userRepository->save($user, true);
-                
-                return $this->redirectToRoute('character_index');
+                return new JsonResponse([
+                    'character' => $character
+                    ] ,200);
         }
 
         return $this->render('character/create.html.twig', [
             'form' => $form->createView()
         ]);
-
-        return new JsonResponse([
-            'character' => $character
-            ] ,200);
     }
 
-    #[Route('/character/update_stats', name: 'character_update_stats')]
-    public function updateStats(Request $request): Response
+    #[Route('/character/update', name: 'character_update')]
+    public function update(Request $request): Response
     {
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
@@ -86,8 +67,7 @@ class CharacterController extends AbstractController
         $spDatabase = $character->getSkillPoints();
         $strPost = $request->request->get('str');
         $spPost  = $request->request->get('sp');
-        if(($spDatabase < $spPost) || ($spDatabase == $spPost) || $spDatabase == 0)
-        {
+        if(($spDatabase < $spPost) || ($spDatabase == $spPost) || $spDatabase == 0) {
             //error message osobno
             //swietny pomysl do checkowania danych
             return new Response("don't do that");
