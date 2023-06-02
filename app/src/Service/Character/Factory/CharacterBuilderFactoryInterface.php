de9f71a5e4b583a9fbefbef18ae34f40d6d41239
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-02 16:31:08 +0200

    create interface

---
+++ app/src/Service/Character/Factory/CharacterBuilderFactoryInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace App\Service\Character\Factory;
+
+use App\Service\Character\Builder\CharacterBuilder;
+
+interface CharacterBuilderFactoryInterface
+{
+    public function createBuilder(int $exp, int $lvl, int $skillPoints): CharacterBuilder;
+}
