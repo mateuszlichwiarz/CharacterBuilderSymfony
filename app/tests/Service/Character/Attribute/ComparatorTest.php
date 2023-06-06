e78ce2d992511b41aedf71348b3d08f9a80e4296
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-06 03:31:01 +0200

    tests: create comparator test

---
+++ app/tests/Service/Character/Attribute/ComparatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\tests\Service\Character\Attribute;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ComparatorTest extends KernelTestCase
+{
+    public function testCompareAttribute()
+    {
+        
+    }
+}
