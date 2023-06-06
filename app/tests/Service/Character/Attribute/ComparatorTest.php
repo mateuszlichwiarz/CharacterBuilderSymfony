ebb0d216381d8164f0ff0c52e8cccb01abe3e63c
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-06 17:08:43 +0200

    tests: exception equals result of comparation

--- app/tests/Service/Character/Attribute/ComparatorTest.php
+++ app/tests/Service/Character/Attribute/ComparatorTest.php
@@ -1,13 +1,33 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\tests\Service\Character\Attribute;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Service\Character\Attribute\Comparator;
+use PHPUnit\Framework\TestCase;
 
-class ComparatorTest extends KernelTestCase
+final class ComparatorTest extends TestCase
 {
-    public function testCompareAttribute()
+    public function testTrueResult(): void
     {
-        
+        $repositoryStrength = 2;
+        $requestStrength    = 1;
+
+        $comparator = new Comparator($repositoryStrength, $requestStrength);
+        $result = $comparator->compareAttribute();
+
+        $this->assertSame($result, true);
+    }
+
+    public function testExceptionMessageFirstCompare(): void
+    {
+        $repositoryStrength = 1;
+        $requestStrength    = 1;
+
+        $comparator = new Comparator($repositoryStrength, $requestStrength);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Nothing to change');
+
+        $comparator->compareAttribute();
     }
 }
