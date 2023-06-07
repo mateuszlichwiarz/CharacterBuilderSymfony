265c3ec490ee9e9231be5f5485760b42dda5b178
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-07 02:07:31 +0200

    delete strength attribute from create form

--- app/src/Form/Character/CreateCharacterFormType.php
+++ app/src/Form/Character/CreateCharacterFormType.php
@@ -8,14 +8,10 @@ use Symfony\Component\Form\AbstractType;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class CreateCharacterFormType extends AbstractType
 {
@@ -23,7 +19,6 @@ class CreateCharacterFormType extends AbstractType
     {
         $builder
             ->add('name', TextType::class)
-            ->add('str', IntegerType::class)
             ->add('save', SubmitType::class)
         ;
     }
