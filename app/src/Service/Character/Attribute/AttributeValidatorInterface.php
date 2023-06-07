fea342fd5101774b648ae28320be61270c21d27c
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-07 04:10:04 +0200

    create interface for validator attributes

---
+++ app/src/Service/Character/Attribute/AttributeValidatorInterface.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace App\Service\Character\Attribute;
+
+interface AttributeValidatorInterface
+{
+    public function validate(int $freePoints, int $attributeRequest, int $attributeRepository): bool;
+}
