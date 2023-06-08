da386f63f8ec51f72f1995f48159168254970ffb
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-09 01:52:02 +0200

    change name and position in directory

--- app/src/Service/Character/Attribute/LevelUp.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-
-class LevelUp
-{
-    public function __construct(){}
-}
---
+++ app/src/Service/Character/Attribute/Lvl/LvlUp.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Service\Character\Attribute\Lvl;
+
+class LvlUp
+{
+    public function __construct(){}
+
+    public function newLvl()
+    {
+        
+    }
+}
