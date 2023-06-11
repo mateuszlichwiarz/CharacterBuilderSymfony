d2451fba3ae1a3e18ad6ca615e8dd77645a0a685
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-11 05:10:06 +0200

    tests: return zero, positive, negative value from diff method

--- app/tests/Service/Character/Attribute/SkillPoints/SkillPointsDiffTest.php
+++ app/tests/Service/Character/Attribute/SkillPoints/SkillPointsDiffTest.php
@@ -1,15 +1,46 @@
 <?php declare(strict_types=1);
 
-namespace App\tests\Service\Character\Attribute\SkillPoints;
+namespace App\tests\Service\Character\Attribute\Strength;
 
 use PHPUnit\Framework\TestCase;
-use App\Service\Character\Attribute\SkillPoints\SkillPointsCalculator;
+use App\Service\Character\Attribute\SkillPoints\SkillPointsDiff;
 use App\Entity\Character;
 
 final class SkillPointsDiffTest extends TestCase
 {
-    public function test(): void
+    public function testDiffResultZeroValue(): void
     {
-        $this->assertTrue(true);
+        $character = new Character();
+        $character->setSkillPoints(10);
+        $inputValue = 10;
+
+        $spDiff = new SkillPointsDiff();
+        $result = $spDiff->getDiff($character, $inputValue);
+
+        $this->assertEquals($result, 0);
+    }
+
+    public function testDiffResultPositiveValue(): void
+    {
+        $character = new Character();
+        $character->setSkillPoints(10);
+        $inputValue = 3;
+
+        $spDiff = new SkillPointsDiff();
+        $result = $spDiff->getDiff($character, $inputValue);
+
+        $this->assertEquals($result, 7);
+    }
+
+    public function testDiffResultNegativeValue(): void
+    {
+        $character = new Character();
+        $character->setSkillPoints(0);
+        $inputValue = 3;
+
+        $spDiff = new SkillPointsDiff();
+        $result = $spDiff->getDiff($character, $inputValue);
+
+        $this->assertEquals($result, -3);
     }
 }
