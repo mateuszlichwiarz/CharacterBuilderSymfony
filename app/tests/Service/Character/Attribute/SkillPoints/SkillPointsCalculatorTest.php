13def77b2fb83a3d92fe9cfe619aa2412c13fdb0
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-11 02:09:12 +0200

    tests: create SkillPoints calculator class test

---
+++ app/tests/Service/Character/Attribute/SkillPoints/SkillPointsCalculatorTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\tests\Service\Character\Attribute\SkillPoints;
+
+use PHPUnit\Framework\TestCase;
+use App\Service\Character\Attribute\SkillPoints\SkillPointsCalculator;
+use App\Entity\Character;
+
+final class SkillPointsCalculatorTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
