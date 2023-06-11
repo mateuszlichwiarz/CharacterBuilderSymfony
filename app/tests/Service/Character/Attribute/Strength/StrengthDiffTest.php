8e642b4b9b496a6978a7e53d5534d3ce4730de13
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-11 04:07:39 +0200

    tests: create Strength diff class test

---
+++ app/tests/Service/Character/Attribute/Strength/StrengthDiffTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\tests\Service\Character\Attribute\Strength;
+
+use PHPUnit\Framework\TestCase;
+use App\Service\Character\Attribute\Strength\StrengthDiff;
+use App\Entity\Character;
+
+final class StrengthDiffTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
