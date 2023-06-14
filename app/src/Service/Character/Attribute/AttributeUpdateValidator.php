b94f2262e7af67beabbe050580bb89d760f79a62
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-14 02:37:17 +0200

    create Attribute update Validator class

---
+++ app/src/Service/Character/Attribute/AttributeUpdateValidator.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace App\Service\Character\Attribute;
+
+use App\Entity\Character;
+use App\Service\Character\Attribute\SkillPoints\SkillPointsAvailable;
+use App\Service\Character\Attribute\SkillPoints\SkillPointsDiff;
+use App\Service\Character\Attribute\Strength\StrengthDiff;
+use App\Service\Character\Attribute\Strength\StrengthValidator;
+
+class AttributeUpdateValidator
+{
+
+    private array $attributes;
+
+    public function __construct(
+        private StrengthDiff $strengthDiff,
+        private SkillPointsDiff $skillPointsDiff,
+        private SkillPointsAvailable $spAvailable,
+        private StrengthValidator $strValidator,
+    )
+    {}
+
+    public function isValid(Character $character): bool
+    {
+        if($this->spAvailable->isAvailable($character) === true) {
+
+            $sumRequestAttrVal = array_sum($this->attributes);
+
+            $sumCharacterAttr = $this->getCharacterAttributesSum($character);
+            $requiredSkillPoints = $sumRequestAttrVal - $sumCharacterAttr;
+
+            if($requiredSkillPoints > $character->getSkillPoints()) {
+                return false;
+            }elseif($requiredSkillPoints <= $character->getSkillPoints() ) {
+                return true;
+            }
+
+        }else
+        {
+            return false;
+        }
+    }
+
+    public function addAttribute(int $attr): self
+    {
+        $this->attributes = [$attr];
+
+        return $this;
+    }
+
+    private function getCharacterAttributesSum(Character $character): int
+    {
+        $dex = $character->getDex();
+        $str = $character->getStr();
+        $wis = $character->getWis();
+
+        return $dex+$str+$wis;
+    }
+}
