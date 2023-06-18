05d670bb7bf25a0a6d2baece362b83caf8e015ea
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-18 03:15:21 +0200

    fix: delete needless use statements

--- app/src/Service/Character/Factory/CharacterBuilderFactory.php
+++ app/src/Service/Character/Factory/CharacterBuilderFactory.php
@@ -5,11 +5,6 @@ namespace App\Service\Character\Factory;
 use App\Service\Character\Factory\CharacterBuilderFactoryInterface;
 use App\Service\Character\Builder\CharacterBuilder;
 
-use App\Entity\Character;
-
-use App\Repository\ArmorRepository;
-use App\Repository\WeaponRepository;
-
 use Psr\Log\LoggerInterface;
 
 class CharacterBuilderFactory implements CharacterBuilderFactoryInterface
