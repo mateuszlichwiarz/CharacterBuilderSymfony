c5f52902b5f4eb83925183f12ee3ccbed4991f6e
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-06-20 21:47:38 +0200

    install apiplatform

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
